9d983d89cffe6757a3018a4c282977a4b083b9fb
Tim La Roche <timlaroche@users.noreply.github.com>
2019-02-16 18:54:23 +0000

    Articles now display algorithmically and can go through to next articles in one direction

--- src/content.jsx
+++ src/content.jsx
@@ -1,36 +1,60 @@
 import React from 'react'
 import axios from 'axios';
 
-/*
-axios.get("http://localhost:3000/article-1").then(function (response){
-	console.log(response.data);
-	ReactDOM.render(
-		<h1> {response.data["title"]} </h1>,
-		document.getElementById('content')
-		);
-});
-*/
-
 export default class ArticleContent extends React.Component {
 
 	constructor(props){
 		super(props)
 		this.state = {
 			isLoaded: false,
-			data: "hello"
+			title: "",
+			body: []
 		};
 	}
 
 	componentDidMount() {
-		console.log("componentDidMount");
+		this.getArticleContents();
+	}
+
+	componentDidUpdate(prevProps, prevState){
+		if(this.props.articleURL != prevProps.articleURL){
+			this.getArticleContents();
+		}
+	}
+
+	getArticleContents(){
 		axios.get(this.props.articleURL).then((response) => {
-			console.log(response.data["title"]);
-			const x = response.data;
-			this.setState({data: x});
+			console.log(response.data);
+			this.setState({title: response.data["title"], body: response.data["body"]});
 		});
 	}
 
 	render(){
-		return <p> {this.state.data["title"]} </p>;
+		return(
+			<React.Fragment>
+			{this.state.body.map((content) => {
+				switch(content.type){
+					case "heading":
+						return <h2> {content.model.text} </h2>
+						break;
+					case "paragraph":
+						return <p> {content.model.text} </p>
+						break;
+					case "image":
+						console.log(content.model.url);
+						return <img src={content.model.url} />
+						break;
+					case "list":
+						if(content.model.type == "unordered"){
+							const items = content.model.items.map((item) => <li> {item} </li> );
+							return <ul> {items} </ul>
+						}
+						break;
+					default:
+						return <p> default </p>
+				}
+			})}
+			</React.Fragment>
+		)
 	}
 }
